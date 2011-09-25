cb81744b8fe77e5af06bf0f938cb47d15333203c
Gaurav <ggmathur@gmail.com>
2011-09-25 00:09:46 -0400

    Added listeners to the sliders (seekbars)
    Check out Settings.java. There are two seekbar listener classes in
    there. Use the onProgressChanged method to do stuff. Don't know what
    the scale is (by default it is 0 to 100).

--- src/com/mattfeury/saucillator/android/Settings.java
+++ src/com/mattfeury/saucillator/android/Settings.java
@@ -4,14 +4,70 @@ import com.sauce.touch.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class Settings extends Activity{
 	
+	SeekBar delaySlider;
+	SeekBar lagSlider;
+	TextView delayValue;
+	TextView lagValue;
+	
+	private class DelaySliderListener implements SeekBar.OnSeekBarChangeListener {
+
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress,
+				boolean fromUser) {
+			// TODO Auto-generated method stub
+			delayValue.setText(" " + progress + " ");
+		}
+
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	private class LagSliderListener implements SeekBar.OnSeekBarChangeListener {
+
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress,
+				boolean fromUser) {
+			// TODO Auto-generated method stub
+			lagValue.setText(" " + progress + " ");
+		}
+
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings);
-		
-		
+		delaySlider = (SeekBar) findViewById(R.id.delaySlider);
+		delaySlider.setOnSeekBarChangeListener(new DelaySliderListener());
+		lagSlider = (SeekBar) findViewById(R.id.lagSlider);
+		lagSlider.setOnSeekBarChangeListener(new LagSliderListener());
+		delayValue = (TextView) findViewById(R.id.delayValue);
+		lagValue = (TextView) findViewById(R.id.lagValue);
 	}
 }
