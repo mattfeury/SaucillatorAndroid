25e5aa861c7c21b1e010031ea1ebe10f57487ec1
Gaurav <ggmathur@gmail.com>
2011-09-24 23:24:54 -0400

    Created the settings menu
    Created a separate view for the settings menu. In the process of adding
    functionality.

---
+++ src/com/mattfeury/saucillator/android/Settings.java
@@ -0,0 +1,17 @@
+package com.mattfeury.saucillator.android;
+
+import com.sauce.touch.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Settings extends Activity{
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
+		
+		
+	}
+}
