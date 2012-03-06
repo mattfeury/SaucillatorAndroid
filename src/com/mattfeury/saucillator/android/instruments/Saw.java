415f1017298f25597c04a6287c75e9f41438234b
Matt Feury <mattfeury@gmail.com>
2012-03-05 23:21:51 -0500

    A sawtooth wave that goes from -amp to amp.
    The Way It Should Be (tm)

--- src/com/mattfeury/saucillator/android/instruments/Saw.java
+++ src/com/mattfeury/saucillator/android/instruments/Saw.java
@@ -19,13 +19,14 @@ public class Saw extends BasicOsc {
   }
 
   public void fill() {
-    float dt = (float)(2.0/ENTRIES);
+    float dt = (amplitude * 2f / ENTRIES);
     int phaseOffset = (int)((float)(oscPhase / (float)360) * ENTRIES);
 
-    // Can't be certain that this phase stuff works because it doesn't
-    // destructively cancel at 180 degrees.
-    for(int i = phaseOffset; i < ENTRIES + phaseOffset; i++) {
-      table[i - phaseOffset] = (float) (i * dt - Math.floor(i * dt));
+    // Cannot gaurantee this phase shift is working,
+    // because the sawtooth is not a mirror image of itself
+    // reflected across a y axis. Therefore, 0 + 180 do not  
+    for(int i = 0; i < ENTRIES; i++) {
+      table[i] = (i + phaseOffset) % ENTRIES * dt - amplitude;
     }
   }
 }
