e1c7f893e04d2f8654e4d7e4c736d31ec9d91cb4
Matt Feury <mattfeury@gmail.com>
2012-08-05 18:37:23 -0400

    Make Limiter scale with the buffer's absolute peak
    We were previously scaling to the max value, under the wrongful
    assumption that the waveform would perfectly mirror itself across the x
    axis. Apparently, that's is not the case. Waveforms get complex, yo.
    This fixes some POPS! MMM FIDELITY.

--- src/com/mattfeury/saucillator/dev/android/sound/Limiter.java
+++ src/com/mattfeury/saucillator/dev/android/sound/Limiter.java
@@ -3,16 +3,19 @@ package com.mattfeury.saucillator.dev.android.sound;
 public class Limiter {
 
   public static void limit(final float[] buffer) {
-    // Determine max value. This could get nasty performance-wise
-    float max = 0;
+    // Determine max absolute value. This could get nasty performance-wise
+    float max = 0, min = 0, peak = 0;
     for (float f : buffer) {
       if (f > max)
         max = f;
+      if (f < min)
+        min = f;
     }
+    peak = Math.max(Math.abs(min), max);
 
-    if (max > 1) {
+    if (peak > 1) {
       for (int i = 0; i < buffer.length; i++)
-        buffer[i] *= 1.0f / max;
+        buffer[i] *= 1f / peak;
     }
   }
 
