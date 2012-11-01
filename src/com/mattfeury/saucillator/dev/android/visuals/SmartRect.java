c1958e743d714219993fc916440269fa0566aa61
Matt Feury <mattfeury@gmail.com>
2012-11-01 12:51:15 -0400

    Have SmartRect implement Drawable
    This gives this class a little more purpose, and rightly so. It is
    entirely a drawable element, and should have base implementations for
    setting position, checking for contains, etc

--- src/com/mattfeury/saucillator/dev/android/visuals/SmartRect.java
+++ src/com/mattfeury/saucillator/dev/android/visuals/SmartRect.java
@@ -1,17 +1,39 @@
 package com.mattfeury.saucillator.dev.android.visuals;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.RectF;
 
 /**
- * Talk about unnecessary. Just a RectF that lets us define it by width and height
+ * Just a RectF that lets us define it by width and height rather than right and bottom measurements.
+ * We also mixin some of our Drawable functionality here since layout elements generally extend this.
  */
-public abstract class SmartRect extends RectF {
+public abstract class SmartRect extends RectF implements Drawable {
+
+  protected Paint defaultPaint = new Paint();
 
   public SmartRect(int x, int y, int width, int height) {
     super(x, y, x + width, y + height);
+
+    defaultPaint.setARGB(0, 0, 0, 0);
   }
 
   public void set(int x, int y, int width, int height) {
     super.set(x, y, x + width, y + height);       
-  }  
+  }
+  
+  public void draw(Canvas canvas) {
+    canvas.drawRect(this, defaultPaint);
+  }
+  public void layoutChanged(int width, int height) {
+    set(left, top, left + width, top + height); 
+  }
+  public boolean contains(int x, int y) {
+    return super.contains(x, y);
+  }
+
+  public boolean shouldClearFloat() {
+    return false;
+  }
+
 }
