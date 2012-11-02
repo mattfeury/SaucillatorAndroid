f6b7d0aeb4ef718f3e0fd0be9a9d143a36c10928
Matt Feury <mattfeury@gmail.com>
2012-11-02 14:10:19 -0400

    Centralize and add some styles for LooperTab buttons

--- src/com/mattfeury/saucillator/dev/android/tabs/LooperTab.java
+++ src/com/mattfeury/saucillator/dev/android/tabs/LooperTab.java
@@ -1,45 +1,57 @@
 package com.mattfeury.saucillator.dev.android.tabs;
 
 import com.mattfeury.saucillator.dev.android.sound.AudioEngine;
+import com.mattfeury.saucillator.dev.android.templates.Button;
 import com.mattfeury.saucillator.dev.android.templates.ButtonBuilder;
 import com.mattfeury.saucillator.dev.android.templates.ClickHandler;
 import com.mattfeury.saucillator.dev.android.utilities.VibratorService;
 
 public class LooperTab extends Tab {
+  
+  private static final int BORDER_SIZE = 5, MARGIN_SIZE = 15;
 
   public LooperTab(final AudioEngine engine) {
     super("Looper", engine);
-    
+
+    Button firstChild = makeLooperButton("Toggle Looper", new ClickHandler() {
+      public void handle(Button button, Object o) {
+        engine.toggleLooperRecording();
+        VibratorService.vibrate();
+
+        button.toggleFocus();
+      }
+    });
+    firstChild.setClear(false);
+
     panel.addChild(
-      ButtonBuilder
-        .build(ButtonBuilder.Type.RECT, "Toggle Looper!")
-        .withHandler(new ClickHandler() {
-          public void handle(Object o) {
-            engine.toggleLooperRecording();
-            VibratorService.vibrate();
-          }
-        })
-        .finish(),
-      ButtonBuilder
-        .build(ButtonBuilder.Type.RECT, "Undo")
-        .withHandler(new ClickHandler() {
-          public void handle(Object o) {
-            engine.undoLooper();
-            VibratorService.vibrate();
-          }
-        })
-        .withClear(true)
-        .finish(),
-      ButtonBuilder
-        .build(ButtonBuilder.Type.RECT, "Reset")
-        .withHandler(new ClickHandler() {
-          public void handle(Object o) {
-            engine.resetLooper();
-            VibratorService.vibrate();
-          }
-        })
-        .withClear(true)
-        .finish()
+      firstChild,
+      makeLooperButton("Undo", new ClickHandler() {
+        public void handle(Button button, Object o) {
+          engine.undoLooper();
+          VibratorService.vibrate();
+        }
+      }),
+      makeLooperButton("Reset", new ClickHandler() {
+        public void handle(Button button, Object o) {
+          engine.resetLooper();
+          VibratorService.vibrate();
+        }
+      })
     );
+  }
+  
+  private Button makeLooperButton(String name, ClickHandler... handlers) {
+    ButtonBuilder builder = ButtonBuilder.build(ButtonBuilder.Type.RECT, name);
+    
+    for (ClickHandler handler : handlers)
+      builder.withHandler(handler);
+
+    return
+      builder
+        .withBorderSize(BORDER_SIZE)
+        .withMargin(MARGIN_SIZE)
+        .withClear(true)
+        .finish();
+
   }
 }
